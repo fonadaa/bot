d41eb0133b2de79f9a32187b1fb1d10cefa45b59
fonadaa <abhishek2@fonada.com>
2024-12-31 11:58:43 +0530

    webhook comment

--- main.js
+++ main.js
@@ -390,7 +390,7 @@ function sendAudioToWebhook(blob) {
             formData.append('User_address', userAddress);
             formData.append('DrBatra_address', nearestClinic.address);
             formData.append('session_id', sessionId); // Add session ID to the request
-
+            // https://fonada.app.n8n.cloud/webhook/dbf05039-6da2-4ffe-b3dc-1cfa03d121ec
             fetch('https://fonada.app.n8n.cloud/webhook-test/2bdfe4c3-d9be-43cc-9f0c-bd14630fc275', {
                 method: 'POST',
                 body: formData
